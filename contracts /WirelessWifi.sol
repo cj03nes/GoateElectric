c6c5cf79d49e37451f2067360cc45f9355afe15a
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-12 08:19:10 -0500

    Update WirelessWifi.sol

--- contracts /WirelessWifi.sol
+++ contracts /WirelessWifi.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ZeropointWifi is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("ZeropointWifi", "ZPW") Ownable(initialOwner) {
+        _mint(initialOwner, 1000000 * 10**2); // Initial supply: 1M $ZPW with 2 decimals
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 2;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
