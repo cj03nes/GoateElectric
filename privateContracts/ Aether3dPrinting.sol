fcbe101d8aa26bb6a862ec478134306b023f338b
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-12 09:42:16 -0500

    Update  Aether3dPrinting.sol

--- privateContracts/ Aether3dPrinting.sol
+++ privateContracts/ Aether3dPrinting.sol
@@ -1,1 +1,54 @@
-cj
+cjcontract WaterToGoldTransfiguration{
+
+// Trial run with real gold
+
+function TurnGoldIntoGold{
+
+// Get the genetics & DNA structure of the gold
+function get the goals genetics{
+
+}
+
+// Boil the gold if no materials
+function boilTheGoal {
+
+}
+
+// Create the gold
+function create goal {
+
+}
+
+function get the goals genetics{
+return green check & bell ding if genetics=goldGenetics
+}}
+
+
+
+function TurnWaterIntoGold{  
+
+//get the genetics of the water
+function get the genetics{}
+
+//get the gold's genetics
+function getTheGoalsGenetics{}
+
+// boil the water
+function boil the water{}
+
+// create the gold
+function create the gold{
+require boiling water => boiling gold
+
+
+require print gold == gold genetics
+}
+
+//get the genetics 
+function get the genetics{
+return green check && bell ding if created goal == gold genetics, else if created goal != gold genetics then return red "x" && cow mooing}
+
+}
+
+
+}
