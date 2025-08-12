c9c87bafb62e66b8dcd8d397067dd3908a2a6c02
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-12 11:43:56 -0500

    Update ZeropointShield.sol

--- contracts /insurance/ZeropointShield.sol
+++ contracts /insurance/ZeropointShield.sol
@@ -1,1 +1,35 @@
+contract ZeropointSHield () {
 
+function ZeropointShield {
+
+color : blue;
+translucent: true;
+permeable: false;
+form: solid;
+
+onContact ={
+if outside force = .push >=< msg.sender then return orbshield;
+
+orbshield is abstract real virtual solid;
+orbshield = (deflect outside force), (catch outside force && encompass outside force && dissolve outside force), (protect ms sender), (defend msg sender), (surround msg.sender);
+
+if outside force = 100 then require(orbshield >= 10,000 );
+
+require collateral force exerted from ( outside force => orbshield) <= msg.sender || 0;
+
+if orbshield is active >= 3s,
+then return orbshield color = clear,
+orbshield defense = maximum,
+orbshield outside view = transparent blue[ front-view && rear view],
+orbshield outside view = 75;
+orbshield inside view = 100;
+orbshield outside audio = 0;
+orbshield inside audio = 100;  }
+
+catch(forceExerted);
+!collateral(rebound forceExerted);
+
+}
+
+
+}
