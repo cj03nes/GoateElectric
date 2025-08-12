4f5aac472709c6d023c3dd30a5cb41a1b928c647
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-12 09:41:01 -0500

    Update GatewayFoodOvenBridge.sol

--- privateContracts/portals/GatewayFoodOvenBridge.sol
+++ privateContracts/portals/GatewayFoodOvenBridge.sol
@@ -1,1 +1,140 @@
-cj
+pragma solidity ^0.8;
+
+// physical digitized industrial ovens & microwaves
+// locationA => locationB
+// from the kitchen to your dinner table, skip the middleman delivery driver
+// no HUMAN life form can enter dead or alive
+// $1 delivery fee, 75% to coreteam, 25% to cj03nes
+import Zeropoint from "./Zeropoint.sol";
+import { connectDevice , deviceConnected, deviceInformation, accountBalances } from "./deviceConnect.sol" ; 
+owner.address = cj03nes;
+
+
+// chain to chain
+mapping(chainA) => mapping(chainB) => crossChain;
+
+// chain to device
+mapping(chain) => mapping(device => mapping(deviceInformation) ) => deviceConnected;
+
+//on-chain Zeropoint to on-chaindevice
+mapping(chain => mapping(uint256 Zeropoint) => mapping(chain => mapping(device => mapping(deviceInformation) ) ) => zeropointConsumedToDevice;
+
+//on-chain ZeropointWifi to on-chain device
+mapping(chain => mapping(uint256 ZeropointWifi) => mapping(chain => mapping(device => mapping(deviceInformation) ) ) => zeropointwifiConsumedToDevice;
+
+// on-chain zeropoint and on-chain zeropointwifi to on-chain device
+mapping(chain => mapping(uint256 Zeropoint) ) && mapping(chain => mapping(uint256 ZeropointWifi) ) => mapping(chain => mapping(device => mapping(deviceInformation) ) ) zeropointAndZeropointWifiConsumedToDevice;
+
+// on-chain zeropoint and zeropointwifi to device location to on-chain zeropoint and zeropointwifi to device location
+mapping(chain => mapping(uint256 Zeropoint) && mapping(chain => mapping(uint256 ZeropointWifi) => mapping(device => mapping(deviceInformation => mapping ( deviceLocation ))))) => mapping(chain => mapping(uint256 Zeropoint) && mapping(chain => mapping(uint256 ZeropointWifi) => mapping(device => mapping(deviceInformation => mapping ( deviceLocation ))))) => deviceLocationAToDeviceLocationB;
+
+
+
+contract gateWayOvenBridge ={
+
+// connect physical doorways
+function connectGateWay ={
+     (require == connectDevice);
+const connectDevice returns deviceConnected;
+
+deviceConnected = gateWayConnected; }
+
+
+// create a bridge between digital doorwayA to digital doorwayB
+function hereToThere = {
+     (require == deviceConnected && gateWayConnected);
+     (require == zeropointAndZeropointWifiConsumedToDevice);
+
+// get the location of digital doorway A
+const chooseLocationA ={
+     (require == deviceConnected && gateWayConnected);
+     (require == zeropointAndZeropointWifiConsumedToDevice);
+     (require == gateWayConnected(deviceInformation[ipAdress && PhoneWifiMACAddress]) );
+}
+// get the location of digital doorway B
+const chooseLocationB ={
+     (require == deviceConnected && gateWayConnected);
+     (require == zeropointAndZeropointWifiConsumedToDevice); 
+     (require == gateWayConnected(deviceInformation[ipAdress && PhoneWifiMACAddress]) );
+
+
+return deviceLocationAToDeviceLocationB; } }
+
+
+
+
+function buyFood ={
+     (require == verifiedChains);
+     (require == verifiedTokenAssets);
+     (require == msg.sender(balance);
+
+if msg.sender(balance) < meal + deliveryFee then return error("Insufficient Funds"),
+else if msg.sender(balance) >= meal + deliveryFee then return mealAndDeliveryFeePaidFor;
+msg.sender(balance) - mealAndDeliveryFeePaidFor = msg.sender(newBalance);
+owner.address(balance) + mealAndDeliveryFeePaidFor = owner.address(newBalance);
+
+
+
+
+newYork ={
+deliveryFee =[
+newYorkToLondon = ( $1 USD ),
+newYorkToCalifornia = ( $1 USD ),
+newYorkToHawaii = ( $1 USD ),
+newYorkToDallas = ( $1 USD ),
+newYorkToNewOrleans = ( $1 USD ),
+newYorkToAtlanta = ( $1 USD ),
+newYorkToFlorida = ( $1 USD ); ]}
+
+
+dallas ={
+deliveryFee =[
+dallasToLondon = ( $1 USD ),
+dallasToCalifornia = ( $1 USD ),
+dallasToHawaii = ( $1 USD ),
+dallasToNewYork = ( $1 USD ),
+dallasToNewOrleans = ( $1 USD ),
+dallasToAtlanta = ( $1 USD ),
+dallasToFlorida = ( $1 USD ); ]}
+
+
+newOrleans ={
+deliveryFee =[
+newOrleansToLondon = ( $1 USD ),
+newOrleansToCalifornia = ( $1 USD ),
+newOrleansToHawaii = ( $1 USD ),
+newOrleansToNewYork = ( $1 USD ),
+newOrleansToDallas = ( $1 USD ),
+newOrleansToAtlanta = ( $1 USD ),
+newOrleansToFlorida = ( $1 USD ); ]}
+
+
+
+atlanta ={
+deliveryFee =[
+atlantaToLondon = ( $1 USD ),
+atlantaToCalifornia = ( $1 USD ),
+atlantaToHawaii = ( $1 USD ),
+atlantaToNewYork = ( $1 USD ),
+atlantaToDallas = ( $1 USD ),
+atlantaToNewOrleans = ( $1 USD ),
+atlantaToFlorida = ( $1 USD ); ]}
+
+
+florida ={
+deliveryFee =[
+floridaToLondon = ( $1 USD ),
+floridaToCalifornia = ( $1 USD ),
+floridaToHawaii = ( $1 USD ),
+floridaToNewYork = ( $1 USD ),
+floridaToDallas = ( $1 USD ),
+floridaToNewOrleans = ( $1 USD ),
+floridaToAtlanta = ( $1 USD ); ]}
+
+
+
+
+}
+
+
+}cj
