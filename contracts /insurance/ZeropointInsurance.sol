fab9a8caa83ce92b236c0622d5edf38c37635cb3
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-12 23:52:58 -0500

    Update ZeropointInsurance.sol

--- contracts /insurance/ZeropointInsurance.sol
+++ contracts /insurance/ZeropointInsurance.sol
@@ -3,50 +3,90 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./ValidationPortal.sol";
 import "./USDMediator.sol";
 import "./InstilledInteroperability.sol";
 
-contract ZeropointGoateInsurance is ERC20, Ownable {
+contract ZeropointInsurance is ERC20, Ownable {
     USDMediator public usdMediator;
     InstilledInteroperability public interoperability;
-    ValidationPortal public validationPortal;
     uint256 public constant SUBSCRIPTION_COST = 6 * 10**18; // $6 in $ZGI
     uint256 public constant SUBSCRIPTION_DURATION = 30 days;
+    uint256 public constant CLAIM_PAYOUT = 100 * 10**18; // Fixed payout amount in $ZGI
 
     struct InsuredDevice {
         string deviceId;
         bool isInsured;
         uint256 expiry;
+        bool shieldActive;
+        uint256 shieldDefenseLevel;
     }
 
     mapping(address => InsuredDevice[]) public insuredDevices;
 
-    constructor(address _usdMediator, address _interoperability, address _validationPortal) 
-        ERC20("ZeropointGoateInsurance", "ZGI") Ownable(msg.sender) {
+    event ShieldActivated(address indexed user, string deviceId, uint256 timestamp);
+    event ShieldDeactivated(address indexed user, string deviceId, uint256 timestamp);
+    event ClaimProcessed(address indexed user, string deviceId, uint256 amount, uint256 timestamp);
+
+    constructor(address _usdMediator, address _interoperability) 
+        ERC20("ZeropointInsurance", "ZGI") Ownable(msg.sender) {
         usdMediator = USDMediator(_usdMediator);
         interoperability = InstilledInteroperability(_interoperability);
-        validationPortal = ValidationPortal(_validationPortal);
         _mint(msg.sender, 1000000 * 10**18);
     }
 
     function subscribe(string memory deviceId, uint256 amount) external {
         require(amount >= SUBSCRIPTION_COST, "Insufficient $ZGI");
         _burn(msg.sender, SUBSCRIPTION_COST);
-        insuredDevices[msg.sender].push(InsuredDevice(deviceId, true, block.timestamp + SUBSCRIPTION_DURATION));
+        insuredDevices[msg.sender].push(InsuredDevice(deviceId, true, block.timestamp + SUBSCRIPTION_DURATION, false, 10000));
     }
 
-    function makeClaim(string memory deviceId, string memory claimData, uint256 amount) external {
-        bool isInsured = false;
-        for (uint i = 0; i < insuredDevices[msg.sender].length; i++) {
-            if (keccak256(bytes(insuredDevices[msg.sender][i].deviceId)) == keccak256(bytes(deviceId)) &&
-                insuredDevices[msg.sender][i].isInsured && insuredDevices[msg.sender][i].expiry > block.timestamp) {
-                isInsured = true;
-                break;
+    function activateShield(string memory deviceId, uint256 outsideForce) external {
+        InsuredDevice[] storage devices = insuredDevices[msg.sender];
+        uint256 index = findDeviceIndex(devices, deviceId);
+        require(index < devices.length, "Device not insured");
+        require(devices[index].isInsured && devices[index].expiry > block.timestamp, "Insurance expired");
+        require(outsideForce >= 100, "Force must be at least 100");
+        require(devices[index].shieldDefenseLevel >= 10000, "Shield strength insufficient");
+
+        devices[index].shieldActive = true;
+        emit ShieldActivated(msg.sender, deviceId, block.timestamp);
+    }
+
+    function deactivateShield(string memory deviceId) external {
+        InsuredDevice[] storage devices = insuredDevices[msg.sender];
+        uint256 index = findDeviceIndex(devices, deviceId);
+        require(index < devices.length, "Device not insured");
+        require(devices[index].shieldActive, "Shield not active");
+
+        devices[index].shieldActive = false;
+        emit ShieldDeactivated(msg.sender, deviceId, block.timestamp);
+    }
+
+    function makeClaim(string memory deviceId, string memory claimData) external {
+        InsuredDevice[] storage devices = insuredDevices[msg.sender];
+        uint256 index = findDeviceIndex(devices, deviceId);
+        require(index < devices.length, "Device not insured");
+        require(devices[index].isInsured && devices[index].expiry > block.timestamp, "Insurance expired");
+
+        // Automatically process claim and transfer payout
+        _mint(msg.sender, CLAIM_PAYOUT);
+        emit ClaimProcessed(msg.sender, deviceId, CLAIM_PAYOUT, block.timestamp);
+
+        // Activate shield upon claim if not already active
+        if (!devices[index].shieldActive) {
+            require(devices[index].shieldDefenseLevel >= 10000, "Shield strength insufficient");
+            devices[index].shieldActive = true;
+            emit ShieldActivated(msg.sender, deviceId, block.timestamp);
+        }
+    }
+
+    function findDeviceIndex(InsuredDevice[] storage devices, string memory deviceId) internal view returns (uint256) {
+        for (uint256 i = 0; i < devices.length; i++) {
+            if (keccak256(bytes(devices[i].deviceId)) == keccak256(bytes(deviceId))) {
+                return i;
             }
         }
-        require(isInsured, "Device not insured");
-        validationPortal.createTask(ValidationPortal.TaskType.InsuranceClaim, claimData);
+        return devices.length;
     }
 
     function getInsuredDevices(address user) external view returns (InsuredDevice[] memory) {
