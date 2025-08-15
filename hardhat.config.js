a1e66cbc2f20f3048deb3d4d2aab238985794d78
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-15 05:09:16 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,12 @@
-// Hardhat configuration for Sepolia deployment
-require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
-require("@nomicfoundation/hardhat-chai-matchers");
-require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.0",
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY || "",
-  },
+      url: process.env.SEPOLIA_RPC_URL || "https://sepolia.infura.io/v3/YOUR_INFURA_KEY",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
