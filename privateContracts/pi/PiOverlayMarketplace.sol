02cafafee3c5d2ac776600434154bb7cab662b88
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-19 23:32:34 -0500

    Update PiOverlayMarketplace.sol

--- privateContracts/pi/PiOverlayMarketplace.sol
+++ privateContracts/pi/PiOverlayMarketplace.sol
@@ -8,7 +8,19 @@ contract Pi overlay marketplace (msg.sender, marketplace, uint256 pi) {
 var p2pListing;
 
 var Amazon marketplace listing;
-// like an ad except you can paywithpi and buy the product through the site
+*/ like an ad except you can paywithpi and buy the product through the site or dropshipping /*
+
+function createPiOverlayWebsite (marketplace, pi checkout, msg.sender(balance)(cart) uint255 pi){
+
+let msg.sender access "marketplace" in Pi Browser;
+upon msg.sender, click cart;
+
+add CheckoutWithPi Modem;
+CheckoutWithPi Modem => Pi Browser, msg.sender cart;
+
+Pi Browser, msg sender cart => PayWithPi || PayWithCrypto || PayWithCard;
+
+}
 
 }
 
