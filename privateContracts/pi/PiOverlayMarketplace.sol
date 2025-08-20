42ed33a24fe103c79abeeefcbe648cf387ac894d
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-19 22:35:44 -0500

    Create PiOverlayMarketplace.sol

---
+++ privateContracts/pi/PiOverlayMarketplace.sol
@@ -0,0 +1,16 @@
+// import pi auth sdk
+import pi tokens & verifiedassets
+
+
+
+contract Pi overlay marketplace (msg.sender, marketplace, uint256 pi) {
+
+var p2pListing;
+
+var Amazon marketplace listing;
+// like an ad except you can paywithpi and buy the product through the site
+
+}
+
+
+}
