76f440a425b1ef4ff7a5709d50eb19c064c8198b
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-19 23:42:44 -0500

    Create PiMigration.sol

---
+++ privateContracts/pi/PiMigration.sol
@@ -0,0 +1,22 @@
+// so pioneers don't have to wait for picoreteam batch migrations
+
+contract Pi migration {
+
+function migratePiFromMining ( kyc'ed user, verified wallet address, mined pi) {
+
+msg.sender(mined pi) => msg.sender(address balance);
+
+upon += msg.sender(address balance), refresh migratable pi to -= mined pi migrated;
+}
+
+
+function migratePiFromValidatingKyc (kyc'ed user, verified wallet address, Validator pi) {
+
+Validator pi = successful validations(number) * 0.33 piForValidation;
+
+msg.sender(Validator pi) => msg.sender(address balance);
+
+upon += msg.sender(address balance), refresh owed pi to -= validator pi migrated;
+}
+
+}
