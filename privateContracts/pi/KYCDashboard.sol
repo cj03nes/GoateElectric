d793f6c72d2f0726f667ba2f61efd9c9a29df8b9
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-19 21:28:35 -0500

    Create KYCDashboard.sol

---
+++ privateContracts/pi/KYCDashboard.sol
@@ -0,0 +1,31 @@
+// as a person that needs to KYC
+
+
+
+function completeKYC () {
+
+var enter information;
+var sign agreement;
+var create wallet && "link to pi browser";
+var enter wallet passphrase for verification;
+var ready to migrate mined tokens;
+
+
+}
+
+
+
+
+__________________________________________________
+
+// as a KYC Validator
+
+function KYCValidating (userKYC information, Validator answer, uint256 pi for kyc) {
+
+var total KYC attempted;
+var total KYC correct;
+var KYC accuracy percentage;
+
+
+}
+
