2e7f73369e7f3a681806fbb32cb08eaaf3f6be12
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-08-19 21:59:26 -0500

    Create PiDAO.sol

---
+++ privateContracts/pi/PiDAO.sol
@@ -0,0 +1,9 @@
+contract PiDAO () {
+
+weight basis: 1 token = 1 vote || 1 verified user = 1 vote;
+vote end clause: Max cap || timer || designated number;
+
+vote reason, "question": delegate to multiple choice || open-ended, come together;
+
+
+}
