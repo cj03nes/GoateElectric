f89d62e02e571e8605ed3c598c34a92868c555a4
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-09-05 16:46:48 -0500

    Create Department Of War/ zeropointLasers.sol

---
+++ privateContracts/Department Of War/ zeropointLasers.sol
@@ -0,0 +1,23 @@
+import Zeropoint.sol
+import WMDRemoteController.sol;
+
+
+contract ZeropointLasers (satellite, target, laser) {
+
+
+require(ZeropointLaser) == blue;
+require(ZeropointLaser) == 628,000,000,000 mW;
+
+1000 watts = 1 kW;
+1000 kW = 1 mW;
+
+// add holographic sight;
+
+function fire {
+ZeropointLaser => target;
+if target speed is >= 100mph,
+then trace target, heat signature;
+}
+
+
+}
