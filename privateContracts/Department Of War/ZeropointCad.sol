7e7ef7f20e1c4cef6e6e23e0c718df6722980805
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-09-05 16:58:49 -0500

    Create ZeropointCad.sol

---
+++ privateContracts/Department Of War/ZeropointCad.sol
@@ -0,0 +1,38 @@
+import Zeropoint.sol;
+import WMDSiphonAndRemoteController sol;
+import ZeropointLaser.sol
+import DeviceConnect.sol;
+import ZeropointShield.sol;
+
+
+contract ZeropointCad (satellite, target device, explosion) {
+
+// cartridge actuated device
+
+require(ZeropointCad) == 628,000,000,000 mW;
+
+1000 watts = 1 kW;
+1000 kW = 1 mW;
+
+function choose device (satellite, device in view, signature) {
+
+signature = Bluetooth address && heat signature;
+
+return device in view => device locked on;
+}
+
+function fire (satellite, device locked on, explosion) {
+
+require(ZeropointCad) => (device locked on);
+
+device locked on, Bluetooth address && heat signature is += ZeropointCad, 628,000,000,000 mW;
+
+require(device locked on), explosion = waterproof;
+
+if explosion is within water,
+then ZeropointShield upon explosion,
+else explosion is explosion;
+}
+
+
+}
