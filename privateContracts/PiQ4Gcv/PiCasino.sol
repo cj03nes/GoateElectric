1ecdf39c7cffb9314e37dc954aca784fb2b82488
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 12:17:10 -0500

    Add PiCasino contract with game imports

--- privateContracts/PiQ4Gcv/PiCasino.sol
+++ privateContracts/PiQ4Gcv/PiCasino.sol
@@ -1,1 +1,17 @@
+pragma solidity v.0.8;
 
+import BlackJack;
+import Roulette;
+import Monopoly;
+import UNO; // from ./goateelectric/contracts/entertainment.sol/
+import Poker;
+import Pity Pat;
+import Spades;
+import ScratchOffs;
+import Lottery;
+
+
+contract PiCasino(games, owner, uint256) {
+
+
+}
