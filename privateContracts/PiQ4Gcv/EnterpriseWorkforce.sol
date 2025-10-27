2c96c67a7eb783b8caac573b2c9c7b4746000a25
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 11:37:01 -0500

    Create EnterpriseWorkforce contract structure
    Define the EnterpriseWorkforce contract with associate and job structures.

--- privateContracts/PiQ4Gcv/EnterpriseWorkforce.sol
+++ privateContracts/PiQ4Gcv/EnterpriseWorkforce.sol
@@ -1,1 +1,21 @@
+pragma solidity v.0.8;
 
+
+contract EnterpriseWorkforce(associate, job, uint256){
+
+associate:(
+account information,
+address,
+selectedCurrency,)
+
+job:(
+hourlyWage,
+schedule,
+hoursThisCycle,
+hoursThisCycle * hourlyWage = CycleEquivalent,
+CycleEquivalent,)
+
+uint256:(
+selectedCurrency,
+CycleEquivalent ÷ selectedCurrency[price] = amount of ($selectedCurrency) to receive), )
+}
