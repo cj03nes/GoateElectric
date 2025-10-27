736c35e256d33cc3bce3eef57aef098c69d764f1
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 15:18:44 -0500

    Update PiNativeInteroperability.sol

--- privateContracts/PiQ4Gcv/PiNativeInteroperability.sol
+++ privateContracts/PiQ4Gcv/PiNativeInteroperability.sol
@@ -1,1 +1,14 @@
+pragma solidity v0.8;
 
+contract PiNativeInteroperability (uint256, mediator, uint256){
+
+uint256:( ),
+mediator:(
+$PI,
+$USDC);
+
+uint256 => $PI => uint256;
+uint256 => $XLM => uint256;
+
+
+}
