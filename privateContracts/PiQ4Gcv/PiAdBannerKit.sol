4ab744ea0029215e2443ed663ba56d81b607f72f
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 11:43:11 -0500

    Add PiAdBannerKit contract with initial setup

--- privateContracts/PiQ4Gcv/PiAdBannerKit.sol
+++ privateContracts/PiQ4Gcv/PiAdBannerKit.sol
@@ -1,1 +1,16 @@
+pragma solidity v.0.8;
 
+import PiAds;
+import PiBlockChain;
+import PiBlockExplorer;
+
+contract PiAdBannerKit (ads, owner, banner){
+
+within scope:("banner "/),
+async function PiAds(ads, views, uint256) {
+
+setup rpc: adSelectionManager/linkTo/ads/viewCount/kycUserName;
+owner address: "YOUR_ADDRESS_HERE";
+}
+
+}
