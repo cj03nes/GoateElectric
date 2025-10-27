be581865b4293141fbf3bbd2cd571f19fcfab00b
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 10:46:34 -0500

    Implement Auction contract with start and end logic
    Added Auction contract with auction lifecycle management.

--- privateContracts/PiQ4Gcv/AuctionUgrade.sol
+++ privateContracts/PiQ4Gcv/AuctionUgrade.sol
@@ -1,1 +1,47 @@
+import PiNFT;
+import PiDomains;
+import PiCollectibles;
+import timestamp, date, timer;
 
+
+
+contract Auction (category, startAuction, endAuction){
+category:(
+domains,
+collectibles, (GOAT/EBAY/FACEBOOKARKETPLACE)
+nft,)
+
+startAuction:(
+beginTime,
+owner address,
+auction item,
+minimum bid,
+auction lasts,)
+
+endAuction:(
+endTime,
+owner address,
+highest bid[asset],
+highest bidder address,
+auction item,
+confirmation,)
+
+comfirmation (owner address, confirmation, highest bidder address{
+
+upon.endAuction(
+return message("Confirm" || "Cancel") to owner address),
+if owner address confirm,
+then transfer highest bidder(highest bid[asset]) to owner address,
+then transfer auctionItem to highest bidder address == new owner, new owner address,
+
+else if owner address cancel,
+then return auction item to owner address,
+then return higjest bid[asset] to highest bidder address;
+
+}
+
+
+
+
+
+}
