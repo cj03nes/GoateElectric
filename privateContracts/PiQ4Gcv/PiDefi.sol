da56a5f678431e5d33402db6d5538d2632fcec87
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 13:45:23 -0500

    Refactor PiDefi contract structure and functions
    Refactor PiDefi contract to include new deposit function and categories.

--- privateContracts/PiQ4Gcv/PiDefi.sol
+++ privateContracts/PiQ4Gcv/PiDefi.sol
@@ -2,10 +2,20 @@ pragma solidity v.0.8;
 
 
 contract PiDefi (msg.sender, category, uint256){
+
 msg.sender:(
-function deposit(msg.sender, address, balance);
+function deposit(msg.sender, address, balance),
+
 category:(
-TokenFarming:(),
-StockFarming:(),
+TokenFarming:(
+($PI, $USD, USDDenomination) is Pi backed by $USD;
+deposit xAmount of $PI in $BTC marketcap, ( $PI, $BTC, $USDDenomination) ),
+
+
+StockFarming:(
+($
+),
 GoldFarming: (),
+
+),
 }
