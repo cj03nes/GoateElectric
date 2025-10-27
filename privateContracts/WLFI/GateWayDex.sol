0f2b5c1c908a97a6ff5c8660e553a5c99c217deb
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 10:33:26 -0500

    Update GateWayDex.sol

--- privateContracts/WLFI/GateWayDex.sol
+++ privateContracts/WLFI/GateWayDex.sol
@@ -1,1 +1,47 @@
+pragma solidity v.0.8;
+
+import GateWayTimeBridge;
+
+contract GateWayDex(){
+
+if GateWayTimeBridge transfer(object) from this.time to that.time,
+then GateWayDex transfer(asset) from this time to that.time;
+
+function swap (this.time[asset], that.time[asset], msg.sender) {
+
+if i have $100 USD && $BTC is $125,000 USD on October 27, 2025,
+if i swap $100 USD for 1000 $BTC for $0.10 USD on November 1, 2015,
+then the $BTC should be sent to msg.sender[address];
+
+{ swap:(
+this.asset,
+this.price,
+this.equivalent,
+this.address,)
+
+  for:(
+that.asset,
+that.price,
+that.equivalent,
+that.exchange,)
+
+transferTo:(
+this.address,
+that.asset,
+this.equivalent == that.equvalent)}
+
+
+
+
+}
+*/ the earliest bitcoin exchanges were :
+
+the btc address itself & bitcoin.com,
+bitstamp,
+mt.gox,
+bitcoin market,
+paypal,
+&etc /*
+}
+
 
