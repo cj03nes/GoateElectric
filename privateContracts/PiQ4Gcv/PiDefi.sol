9ca3b700e15493f90d32064ce5eb25f810702769
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 13:39:42 -0500

    Create PiDefi contract with deposit functionality
    Define PiDefi contract with deposit function and categories.

--- privateContracts/PiQ4Gcv/PiDefi.sol
+++ privateContracts/PiQ4Gcv/PiDefi.sol
@@ -1,1 +1,11 @@
+pragma solidity v.0.8;
 
+
+contract PiDefi (msg.sender, category, uint256){
+msg.sender:(
+function deposit(msg.sender, address, balance);
+category:(
+TokenFarming:(),
+StockFarming:(),
+GoldFarming: (),
+}
