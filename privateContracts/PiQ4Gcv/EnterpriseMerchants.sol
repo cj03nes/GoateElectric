f6d5d45ff7448567b60f7ebc37279a4f49a41593
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 11:29:11 -0500

    Update EnterpriseMerchants.sol

--- privateContracts/PiQ4Gcv/EnterpriseMerchants.sol
+++ privateContracts/PiQ4Gcv/EnterpriseMerchants.sol
@@ -1,1 +1,32 @@
+pragma soldity v.0.8;
 
+// price goods in crypto
+// accept & store assets
+// swap (PiDex) & withdraw 
+// transfer & pay (EnterpriseWorkforce)
+
+contract EnterpriseMerchant (){
+
+async function UsdDenominationPrice (price in usd, address, uint256 amount) {
+// get the grocery cart price in usd
+// select which crypto to pay with
+// transfer agreed payment
+// print receipt
+}
+
+async function store (recipient, owner, uint256){
+// receive asset
+}
+
+function swap (asset, owner, uint256) {
+swap(PiDex);
+}
+
+function transfer(owner, recipient, uint256){
+}
+
+scheduled function payWorkers(owner, associate, uint256){
+pay(EnterpriseWorkForce);
+}
+
+}
