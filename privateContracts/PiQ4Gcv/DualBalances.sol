4e114f3a0ce76a36e3f99202b559584ae3030585
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 11:07:06 -0500

    Update DualBalances.sol

--- privateContracts/PiQ4Gcv/DualBalances.sol
+++ privateContracts/PiQ4Gcv/DualBalances.sol
@@ -1,1 +1,19 @@
+pragma solidity v.0.8;
 
+import PiBlockchain;
+import PiBlockExplorer;
+import Pi;
+import PiDualTx;
+import PiPurityBadge;
+import PiNexus/GcvLiquidity;
+
+mapping(uint256) Pi;
+mapping(PiPurityBadge) => mapping(Pi) GcvPi;
+
+contract DualBalances (pi market balance, pi gcv balance, uint256) {
+
+async function store () {}
+async function view () {}
+
+
+}
