578e2286821725fb445bd52f036f6a6797d05eb4
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 20:32:54 -0500

    Create GateWayData contract with pricefeed details
    Define GateWayData contract with pricefeed parameters.

--- privateContracts/WLFI/GateWayData.sol
+++ privateContracts/WLFI/GateWayData.sol
@@ -1,1 +1,26 @@
+pragma solidity v.0.8;
 
+
+contract GateWayData( current pricefeed, future pricefeed, uint256) {
+require(GateWayBridge);
+require(GateWayTimeBridge);
+
+current pricefeed:(
+chainlink pricefeed & oracles,
+viewer => btcchainexplorer, etherscan, coinmarketcap, coingecko,
+onchain data,
+contract addresses,
+),
+future pricefeeds:(
+chainlink pricefeed & oracles,
+viewer => btcchainexplorer, etherscan, coinmarketcap, coingecko,
+onchain data,
+contract addresses,
+),
+uint256:(
+dateDeployed,
+currentDate,
+futureDate,
+),
+
+}
