7a67a58ca5528066ebf0d5f3af2624b38d7640bf
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 20:18:10 -0500

    Add FederalMonthlyStimulus contract with parameters

--- privateContracts/WLFI/FederalMonthlyStimulus.sol
+++ privateContracts/WLFI/FederalMonthlyStimulus.sol
@@ -1,1 +1,31 @@
+pragma solidity v.0.8;
 
+contract FederalMonthlyStimulus(reserve, monthly wage, constituent){
+
+reserve:(
+$USD,
+$BTC,
+$STOCKS,
+$SILVER,
+$OIL,
+),
+monthly wage:(
+reserve monthly +% , 100%;
+split 20% between comstituents),
+constituents:(
+name,
+address,
+date of birth,
+social security number,
+),
+select to receive in:(
+$BTC,
+$WLFI
+$USD,
+$STOCKS,
+$PI,
+$XLM,
+),
+
+
+}
