01baafbcdb1935df5ff0c6796ce2533a71818bd9
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-10-27 20:09:03 -0500

    Implement BankingATM contract with financial functions
    Added functions for deposit, withdraw, sell, and buy gold in BankingATM contract.

--- privateContracts/WLFI/BankingATM.sol
+++ privateContracts/WLFI/BankingATM.sol
@@ -1,1 +1,49 @@
-WLFI
+pragma solidity v.0.8;
+
+import ZeropointWifi;
+
+contract BankingATM (card, user, uint256){
+
+function deposit fiat(card, fiat, uint256){
+
+if msg.sender deposit fiat,
+then the $amount in $USD = the usd $amount in select crypto;
+}
+
+function withdraw fiat(card, fiat, uint256){
+
+if msg.sender withdraw fiat,
+then the usd $amount in msg.sender[balance] = the usd $amount to dispense;
+}
+
+
+function sell gold(weight, identification, uint256){
+
+if msg.sender sells gold,
+then get:(
+gold[weight],
+gold[purity],
+gold[pricefeed],
+
+gold[value in $USD]{}
+
+}
+
+function buy gold(uint256 insert, identification, uint256 dispense) {
+
+uint256 insert:(
+card,
+fiat,
+),
+identification:(
+id,
+license,
+),
+dispense:(
+gold,
+silver,
+copper,
+),
+}
+
+}
