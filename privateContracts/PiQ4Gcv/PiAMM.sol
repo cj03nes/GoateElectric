44477f2b9598d2a6b04d14eb9b23a2625c646d57
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-11-02 02:08:03 -0600

    Create PiAMM.sol with initial contract structure
    Initial implementation of PiAMM contract with imports and structure.

---
+++ privateContracts/PiQ4Gcv/PiAMM.sol
@@ -0,0 +1,27 @@
+pragma solodoty v.0.8;
+
+import chainlink pricefeeds;
+import {verifiedTokens, verifiedChains} from InstilledaInteroperability.sol;
+import {PiDex[Pi(native)], PiDex[Ethereum], PiDex[Binance], PiDex[Stellar], and etc from PiDex & add to InstilledInteroperability too;
+
+(chain, token):(
+contract addresses,
+token name,
+token symbol,
+token decimal,
+token circulating supply,
+token total supply,
+token max suplply,
+token marketcap,
+token marketcap ÷ circulating supply token price,
+token price,
+
+
+
+contract PiAMM (msg.sender, verifiedToken, uint256){
+
+function store(){}
+
+function
+
+}
