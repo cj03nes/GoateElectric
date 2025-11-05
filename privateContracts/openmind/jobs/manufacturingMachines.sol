9f25b12c5157d498f7ffd2fd9fb548141d48cc21
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-11-05 12:14:21 -0600

    Add manufacturingMachines contract with machines and products

--- privateContracts/openmind/jobs/manufacturingMachines.sol
+++ privateContracts/openmind/jobs/manufacturingMachines.sol
@@ -1,1 +1,19 @@
+pragma solidity v.0.8;
 
+import ai;
+
+contract manufacturingMachines(machine, ai, product){
+
+machine:(
+3d printer,
+printing press,
+sewing machine,
+),
+
+product:(
+clothes,
+cars,
+phones,
+),
+
+}
