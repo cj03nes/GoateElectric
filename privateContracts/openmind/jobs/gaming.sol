e9493a2837292b94b1fb0ea1d4cf528cff027fa5
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-11-05 12:09:58 -0600

    Add aiGaming contract with game and level options

--- privateContracts/openmind/jobs/gaming.sol
+++ privateContracts/openmind/jobs/gaming.sol
@@ -1,1 +1,23 @@
+pragma solidity v.0.8;
 
+import ai;
+
+contract aiGaming(game, ai, level){
+
+game:(
+cards,
+fortnite,
+call of duty,
+// "party fillers"
+),
+
+level:(
+easy,
+medium,
+hard,
+extra hard,
+veteran,
+),
+
+
+}
