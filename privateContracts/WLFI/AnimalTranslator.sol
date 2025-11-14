cf2a3244fd66cda00730a7282b26fdcef3ff2f10
cj03nes <72360646+cj03nes@users.noreply.github.com>
2025-11-14 14:11:17 -0600

    Create AnimalTranslator.sol

---
+++ privateContracts/WLFI/AnimalTranslator.sol
@@ -0,0 +1,33 @@
+pragma solidity v.0.8;
+
+import Zeropoint; // aether
+import ZeropointWifi;
+
+
+contract Animal Transalator (animal, owner, launage){
+
+animal:(
+dog,
+cat,
+bird,
+horse,
+penguin,
+wolf,
+bear,
+),
+owner:(
+msg.sender(),
+),
+language:(
+english,
+spanish,
+french,
+russian,
+
+
+),
+),
+
+upon.animal(speak),
+.translateTo language(english),
+}
